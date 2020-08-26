c9b5545f25df264c9fb6be2336c3c3a427777786
Wilfried Pereira <wilfried.pereira5@gmail.com>
2020-08-26 21:23:01 +0200

    refactor(spotify): WIP split controller/service

--- server/spotify-manager/src/controllers/spotify-manager.controller.ts
+++ server/spotify-manager/src/controllers/spotify-manager.controller.ts
@@ -1,29 +1,22 @@
-import { Controller, Get, Logger, HttpService, Req, Post, Delete, flatten } from '@nestjs/common';
+import { Controller, Get, Logger, HttpService, Req, Post, Delete } from '@nestjs/common';
 import { Request } from 'express';
-import { Observable, EMPTY, Subject } from 'rxjs';
-import { map, flatMap, expand, takeWhile, scan, tap, bufferWhen } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, flatMap, scan } from 'rxjs/operators';
 
+import { environment } from 'environment';
 import { SpotifyManagerService } from 'src/services/spotify-manager.service';
-
 import { SpotifyConfiguration, SpotifyUser, SpotifyPlaylist, SpotifyPaging, PlaylistTrack } from 'src/models/spotify.models';
 
 @Controller('spotify')
 export class SpotifyManagerController {
 
     private logger = new Logger('Spotify Manager Controller');
-    private baseApiUrl: string = 'https://api.spotify.com/v1';
+    private baseApiUrl: string = environment.apiBaseUrl;
 
     constructor(
         private http: HttpService,
         private spotifyService: SpotifyManagerService
     ) { }
-
-    private getAuthorizationHeader(request: Request): any {
-        return {
-            Authorization: request.headers['authorization'] ? request.headers['authorization'] : null,
-            'Content-Type': 'application/json'
-        };
-    }
 
     @Get('configuration')
     getSpotifyConfiguration(): Observable<SpotifyConfiguration> {
@@ -34,7 +27,7 @@ export class SpotifyManagerController {
     @Get('me')
     getUserProfile(@Req() request: Request): Observable<SpotifyUser> {
         this.logger.log(`Requesting user's profile`);
-        return this.http.get<SpotifyUser>(`${this.baseApiUrl}/me`, { headers: this.getAuthorizationHeader(request) })
+        return this.http.get<SpotifyUser>(`${this.baseApiUrl}/me`, { headers: this.spotifyService.getAuthorizationHeader(request) })
             .pipe(map(response => response.data));
     }
 
@@ -43,9 +36,10 @@ export class SpotifyManagerController {
         return this.getUserProfile(request)
             .pipe(flatMap(user => {
                 this.logger.log(`Requesting playlists for user: ${user.id}`);
+                console.log('DQOPZJFOIQZJDIOZQJFJOQZJDFOPQZOP');
                 return this.http.get<SpotifyPaging<SpotifyPlaylist>>(
                     `${this.baseApiUrl}/users/${user.id}/playlists`,
-                    { headers: this.getAuthorizationHeader(request) }
+                    { headers: this.spotifyService.getAuthorizationHeader(request) }
                 ).pipe(map(response => response.data));
             }));
     }
@@ -55,16 +49,14 @@ export class SpotifyManagerController {
         this.logger.log(`Requesting playlist with ID: ${request.params.id}`);
         return this.http.get<SpotifyPlaylist>(
             `${this.baseApiUrl}/playlists/${request.params.id}`,
-            { headers: this.getAuthorizationHeader(request) }
+            { headers: this.spotifyService.getAuthorizationHeader(request) }
         ).pipe(map(response => response.data));
     }
 
     @Get('playlists/:id/tracks')
     getPlaylistTracks(@Req() request: Request): Observable<SpotifyPaging<PlaylistTrack>> {
         this.logger.log(`Requesting tracks from playlist: ${request.params.id}`);
-        return this.getTracksByRequest(request).pipe(
-            expand(tracks => this.getTracksByNext(tracks.next, this.getAuthorizationHeader(request))),
-            takeWhile(tracks => Boolean(request.query.search) && Boolean(tracks.next), true),
+        return this.spotifyService.getCompleteTracklist(request).pipe(
             scan((prev, next) => ({ ...next, items: [...prev.items, ...next.items] })),
             map(tracks => request.query.search
                 ? ({
@@ -76,25 +68,12 @@ export class SpotifyManagerController {
             ));
     }
 
-    private getTracksByRequest(request: Request): Observable<SpotifyPaging<PlaylistTrack>> {
-        return this.http.get<SpotifyPaging<PlaylistTrack>>(
-            request.query.next
-                ? Buffer.from(request.query.next.toString(), 'base64').toString()
-                : `${this.baseApiUrl}/playlists/${request.params.id}/tracks?offset=0&limit=100`,
-            { headers: this.getAuthorizationHeader(request) }).pipe(map(response => response.data));
-    }
-
-    private getTracksByNext(next: string, authorization: string): Observable<SpotifyPaging<PlaylistTrack>> {
-        return this.http.get<SpotifyPaging<PlaylistTrack>>(next, { headers: authorization })
-            .pipe(map(response => response.data));
-    }
-
     @Post('users/:id/playlists')
     createPlaylist(@Req() request: Request): Observable<SpotifyPlaylist> {
         this.logger.log(`Creating a new playlist for user: ${request.params.id}`);
         return this.http.post<SpotifyPlaylist>(
             `${this.baseApiUrl}/users/${request.params.id}/playlists`, JSON.stringify(request.body),
-            { headers: this.getAuthorizationHeader(request) }
+            { headers: this.spotifyService.getAuthorizationHeader(request) }
         ).pipe(map(response => response.data));
     }
 
@@ -102,79 +81,16 @@ export class SpotifyManagerController {
     addTracks(@Req() request: Request): Observable<never> {
         this.logger.log(`Adding new tracks in: ${request.params.id}`);
         return request.query.from
-            ? this.getTracksToAdd(request, request.query.from.toString())
-            : this.addTracksByRequest(request);
+            ? this.spotifyService.getTracksToAdd(request, request.query.from.toString())
+            : this.spotifyService.addTracksByRequest(request);
     }
 
     @Delete('/playlists/:id')
     removeTracks(@Req() request: Request): Observable<never> {
         this.logger.log(`Removing tracks from: ${request.params.id}`);
         return request.query.from
-            ? this.getTracksToRemove(request, request.query.from.toString())
-            : this.removeTracksByRequest(request);
-    }
-
-    private getTracksToAdd(request: Request, playlist: string): Observable<never> {
-        return this.getCompleteTracklist(request, playlist).pipe(
-            map(tracks => tracks.items.map(item => item.track.uri)),
-            map(uris => ({ uris: [...uris] })),
-            flatMap(tracklist => this.addTracksByRequest(request, true, tracklist))
-        )
-    }
-
-    private addTracksByRequest(request: Request, complete?: boolean, tracklist?: { uris: string[] }): Observable<never> {
-        return this.http.post<never>(
-            `${this.baseApiUrl}/playlists/${request.params.id}/tracks`,
-            complete ? JSON.stringify(tracklist) : JSON.stringify(request.body),
-            { headers: this.getAuthorizationHeader(request) }
-        ).pipe(flatMap(() => EMPTY));
-    }
-
-    private getTracksToRemove(request: Request, playlist: string): Observable<never> {
-        const closingEvent: Subject<boolean> = new Subject<boolean>();
-        return this.getCompleteTracklist(request, playlist).pipe(
-            tap(tracks => this.toggleClosingBuffer(tracks, closingEvent)),
-            map(tracks => tracks.items.map(item => item.track.uri)),
-            bufferWhen(() => closingEvent.asObservable()),
-            map(buffer => flatten(buffer)),
-            map(uris => this.spotifyService.formatTracksToRemove(uris)),
-            flatMap(tracklist => tracklist.pipe(
-                flatMap(list => this.removeTracksByRequest(request, true, list))
-            ))
-        )
-    }
-
-    private removeTracksByRequest(
-        request: Request,
-        complete?: boolean,
-        tracklist?: { tracks: { uri: string }[] }
-    ): Observable<never> {
-        return this.http.delete<never>(
-            `${this.baseApiUrl}/playlists/${request.params.id}/tracks`,
-            {
-                data: complete
-                    ? JSON.stringify(tracklist)
-                    : JSON.stringify(this.spotifyService.formatTracksToRemove(request.body)),
-                headers: this.getAuthorizationHeader(request)
-            }
-        ).pipe(flatMap(() => EMPTY));
-    }
-
-    private getCompleteTracklist(request: Request, playlist: string): Observable<SpotifyPaging<PlaylistTrack>> {
-        return this.http.get<SpotifyPaging<PlaylistTrack>>(
-            `${this.baseApiUrl}/playlists/${playlist}/tracks?offset=0&limit=100`,
-            { headers: this.getAuthorizationHeader(request) }
-        ).pipe(
-            map(response => response.data),
-            expand(tracks => this.getTracksByNext(tracks.next, this.getAuthorizationHeader(request))),
-            takeWhile(tracks => Boolean(tracks.next), true),
-        );
-    }
-
-    private toggleClosingBuffer(tracks: SpotifyPaging<PlaylistTrack>, closingEvent: Subject<boolean>): void {
-        if (!tracks.next && !tracks.previous) {
-            closingEvent.next(true);
-        }
+            ? this.spotifyService.getTracksToRemove(request, request.query.from.toString())
+            : this.spotifyService.removeTracksByRequest(request);
     }
 
 }
--- server/spotify-manager/src/services/spotify-manager.service.ts
+++ server/spotify-manager/src/services/spotify-manager.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService, flatten } from '@nestjs/common';
 import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
-import { Observable, from } from 'rxjs';
+import { Request } from 'express';
+import { Observable, from, EMPTY, Subject } from 'rxjs';
+import { map, flatMap, tap, bufferWhen, expand, takeWhile } from 'rxjs/operators';
 
-import { SpotifyConfiguration, PlaylistTrack } from 'src/models/spotify.models';
+import { environment } from 'environment';
+import { SpotifyConfiguration, PlaylistTrack, SpotifyPaging } from 'src/models/spotify.models';
 
 @Injectable()
 export class SpotifyManagerService {
 
     private client: ClientProxy;
+    private baseApiUrl: string = environment.apiBaseUrl;
 
-    constructor() {
+    constructor(private http: HttpService) {
         this.client = ClientProxyFactory.create({
             transport: Transport.TCP,
             options: {
@@ -22,6 +26,87 @@ export class SpotifyManagerService {
     getSpotifyConfiguration(): Observable<SpotifyConfiguration> {
         // Sending empty string because there is no need to send any data
         return this.client.send('spotifyConfiguration', '');
+    }
+
+    getAuthorizationHeader(request: Request): any {
+        return {
+            Authorization: request.headers['authorization'] ? request.headers['authorization'] : null,
+            'Content-Type': 'application/json'
+        };
+    }
+
+    getTracksByRequest(request: Request): Observable<SpotifyPaging<PlaylistTrack>> {
+        return this.http.get<SpotifyPaging<PlaylistTrack>>(
+            request.query.next
+                ? Buffer.from(request.query.next.toString(), 'base64').toString()
+                : `${this.baseApiUrl}/playlists/${request.params.id}/tracks?offset=0&limit=100`,
+            { headers: this.getAuthorizationHeader(request) }).pipe(map(response => response.data));
+    }
+
+    getTracksByNext(next: string, authorization: string): Observable<SpotifyPaging<PlaylistTrack>> {
+        return this.http.get<SpotifyPaging<PlaylistTrack>>(next, { headers: authorization })
+            .pipe(map(response => response.data));
+    }
+
+    getTracksToAdd(request: Request, playlist: string): Observable<never> {
+        return this.getCompleteTracklist(request).pipe(
+            map(tracks => tracks.items.map(item => item.track.uri)),
+            map(uris => ({ uris: [...uris] })),
+            flatMap(tracklist => this.addTracksByRequest(request, true, tracklist))
+        )
+    }
+
+    addTracksByRequest(request: Request, complete?: boolean, tracklist?: { uris: string[] }): Observable<never> {
+        return this.http.post<never>(
+            `${this.baseApiUrl}/playlists/${request.params.id}/tracks`,
+            complete ? JSON.stringify(tracklist) : JSON.stringify(request.body),
+            { headers: this.getAuthorizationHeader(request) }
+        ).pipe(flatMap(() => EMPTY));
+    }
+
+    getTracksToRemove(request: Request, playlist: string): Observable<never> {
+        const closingEvent: Subject<boolean> = new Subject<boolean>();
+        return this.getCompleteTracklist(request).pipe(
+            tap(tracks => this.toggleClosingBuffer(tracks, closingEvent)),
+            map(tracks => tracks.items.map(item => item.track.uri)),
+            bufferWhen(() => closingEvent.asObservable()),
+            map(buffer => flatten(buffer)),
+            map(uris => this.formatTracksToRemove(uris)),
+            flatMap(tracklist => tracklist.pipe(
+                flatMap(list => this.removeTracksByRequest(request, true, list))
+            ))
+        )
+    }
+
+    removeTracksByRequest(
+        request: Request,
+        complete?: boolean,
+        tracklist?: { tracks: { uri: string }[] }
+    ): Observable<never> {
+        return this.http.delete<never>(
+            `${this.baseApiUrl}/playlists/${request.params.id}/tracks`,
+            {
+                data: complete
+                    ? JSON.stringify(tracklist)
+                    : JSON.stringify(this.formatTracksToRemove(request.body)),
+                headers: this.getAuthorizationHeader(request)
+            }
+        ).pipe(flatMap(() => EMPTY));
+    }
+
+    getCompleteTracklist(request: Request): Observable<SpotifyPaging<PlaylistTrack>> {
+        return this.getTracksByRequest(request).pipe(
+            expand(tracks => this.getTracksByNext(tracks.next, this.getAuthorizationHeader(request))),
+            takeWhile(tracks => request.query.search
+                ? Boolean(request.query.search) && Boolean(tracks.next)
+                : Boolean(tracks.next), true)
+        );
+    }
+
+    private toggleClosingBuffer(tracks: SpotifyPaging<PlaylistTrack>, closingEvent: Subject<boolean>): void {
+        if (!tracks.next && !tracks.previous) {
+            closingEvent.next(true);
+        }
     }
 
     formatTracksToRemove(tracks: string[]): Observable<{ tracks: { uri: string }[] }> {
